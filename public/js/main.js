d946f25c32b6a08e02a6d2270ef0a21b696bdb87
MasayaMorinaga <m.morinaga0@gmail.com>
2019-08-22 20:27:47 +0900

    AboutMeあたり書いた

--- public/js/main.js
+++ public/js/main.js
@@ -1,57 +1,42 @@
-// smooth-scroll
-$.smoothScroll({
-    //滑动到的位置的偏移量
-    offset: 0,
-    //滑动的方向，可取 'top' 或 'left'
-    direction: 'top',
-    // 只有当你想重写默认行为的时候才会用到
-    scrollTarget: null,
-    // 滑动开始前的回调函数。`this` 代表正在被滚动的元素
-    beforeScroll: function () { },
-    //滑动完成后的回调函数。 `this` 代表触发滑动的元素
-    afterScroll: function () { },
-    //缓动效果
-    easing: 'swing',
-    //滑动的速度
-    speed: 700,
-    // "自动" 加速的系数
-    autoCoefficent: 2
-});
+// Flyout Menu Functions
+var flyoutmenus = {
+  ".lang-toggle": "#lang-menu",
+  ".share-toggle": "#share-menu",
+  ".nav-toggle": "#site-nav-menu"
+};
 
-
-// Bind the hashchange event listener
-$(window).bind('hashchange', function (event) {
-    $.smoothScroll({
-        // Replace '#/' with '#' to go to the correct target
-        offset: $("body").attr("data-offset")? -$("body").attr("data-offset"):0 ,
-        // offset: -30,
-        scrollTarget: decodeURI(location.hash.replace(/^\#\/?/, '#'))
-        
-      });
-});
-
-// $(".smooth-scroll").on('click', "a", function() {
-$('a[href*="#"]')
-    .bind('click', function (event) {    
-    // Remove '#' from the hash.
-    var hash = this.hash.replace(/^#/, '')
-    if (this.pathname === location.pathname && hash) {
-        event.preventDefault();
-        // Change '#' (removed above) to '#/' so it doesn't jump without the smooth scrolling
-        location.hash = '#/' + hash;
+$.each(flyoutmenus, function( key, value ) {
+  $(key).on("click", function() {
+    $(".flyout-menu").not($(value + ".flyout-menu")).removeClass("active");
+    if ($("#wrapper").hasClass('overlay') && $(value).hasClass('active')) {
+      $("#wrapper").removeClass('overlay');
+    } else {
+      $("#wrapper").addClass('overlay');
     }
+    $(value).toggleClass('active');
+  });
 });
 
-// Trigger hashchange event on page load if there is a hash in the URL.
-if (location.hash) {
-    $(window).trigger('hashchange');
-}
 
-// // $('[data-spy="scroll"]').each(function () {
-// //     var $spy = $(this).scrollspy('refresh')
-// //   })
+// Click anywhere outside a flyout to close
+$(document).on("click", function(e) {
+  if ($(e.target).is(".lang-toggle, .lang-toggle span, #lang-menu, .share-toggle, .share-toggle i, #share-menu, .search-toggle, search.toggle i, #search-menu, .nav-toggle, .nav-toggle i, #site-nav") === false) {
+    $(".flyout-menu").removeClass("active");
+    $("#wrapper").removeClass('overlay');
+  }
+});
 
-// $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
-//     // do something…
-//     var offset = $('[data-spy="scroll"]').attr("data-offset")
-//   })
+// Check to see if the window is top if not then display button
+$(window).scroll(function() {
+  if ($(this).scrollTop()) {
+    $('#back-to-top').fadeIn();
+  } else {
+    $('#back-to-top').fadeOut();
+  }
+});
+
+// Click event to scroll to top
+$('#back-to-top').click(function() {
+  $('html, body').animate({scrollTop: 0}, 1000);
+  return false;
+});
